39c35e296ad1ea755cb31ff58b782b07d6f4337f
legregius <legregius@99ea70e1-9a43-0410-b3df-a6f06dae6e96>
2009-05-18 16:33:13 +0000

    Making all objects never go to sleep is really bad for performance.

--- pal_i/bullet_pal.cpp
+++ pal_i/bullet_pal.cpp
@@ -568,7 +568,9 @@ void palBulletBodyBase::BuildBody(const palVector3& pos, Float mass,
 
    m_pbtBody = new btRigidBody(mass,m_pbtMotionState,pShape,localInertia);
    m_pbtBody->setUserPointer(dynamic_cast<palBodyBase*>(this));
-   m_pbtBody->setActivationState(DISABLE_DEACTIVATION);
+   // Disabling deactivition is really bad.  objects will never go to sleep. which is bad for
+   // performance
+   //m_pbtBody->setActivationState(DISABLE_DEACTIVATION);
 
    int currFlags = m_pbtBody->getCollisionFlags();
 
