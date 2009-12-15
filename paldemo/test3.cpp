5381ce0a1d7eb0f71c7979a5dc3b4be210efa706
brockwoolf <brockwoolf@99ea70e1-9a43-0410-b3df-a6f06dae6e96>
2009-12-15 10:28:27 +0000

    Fixed a problem where the PAL API had been updated, but test3.cpp still used the old API calls.

--- paldemo/test3.cpp
+++ paldemo/test3.cpp
@@ -10,43 +10,127 @@ void Test_3::InitMaterials() {
 			printf("could not create materials!\n");
 			return;
 		}
+
+		// ------------ Real materials ------------
+		// Author: Brock Woolf (brock.woolf@gmail.com)
+		// Values referenced from here: http://www.roymech.co.uk/Useful_Tables/Tribology/co_of_frict.htm
 		
-		//real materials
-		pm->NewMaterial("Aluminum", 1.05, 1.4,0);
-		pm->NewMaterial("Diamond", 0.1, 0.05, 0);
-		pm->NewMaterial("Oak", 0.54, 0.32, 0);
-		pm->NewMaterial("Steel", 0.74, 0.57, 0);
-		pm->NewMaterial("Wood",0.2,0.1,0.1);
+		// Aluminium
+		matDesc_Aluminium.m_fStatic = 1.05f;
+		matDesc_Aluminium.m_fKinetic = 1.40f;
+		matDesc_Aluminium.m_fRestitution = 0.0f;
+		// Diamond
+		matDesc_Diamond.m_fStatic = 0.1f;
+		matDesc_Diamond.m_fKinetic = 0.05f;
+		matDesc_Diamond.m_fRestitution = 0.0f;
+		// Oak
+		matDesc_Oak.m_fStatic = 0.54f;
+		matDesc_Oak.m_fKinetic = 0.32f;
+		matDesc_Oak.m_fRestitution = 0.0f;
+		// Steel
+		matDesc_Steel.m_fStatic = 0.74f;
+		matDesc_Steel.m_fKinetic = 0.57f;
+		matDesc_Steel.m_fRestitution = 0.0f;
+		// Wood
+		matDesc_Wood.m_fStatic = 0.2f;
+		matDesc_Wood.m_fKinetic = 0.1f;
+		matDesc_Wood.m_fRestitution = 0.1f;
+		// Aluminum-Steel
+		matDesc_Aluminum_Steel.m_fStatic = 0.61f;
+		matDesc_Aluminum_Steel.m_fKinetic = 0.47f;
+		matDesc_Aluminum_Steel.m_fRestitution = 0.0f;
 
-		pm->SetMaterialInteraction("Aluminum", "Steel", 0.61, 0.47, 0);
+		pm->NewMaterial("Aluminum", matDesc_Aluminium);
+		pm->NewMaterial("Diamond", matDesc_Diamond);
+		pm->NewMaterial("Oak", matDesc_Oak);
+		pm->NewMaterial("Steel", matDesc_Steel);
+		pm->NewMaterial("Wood", matDesc_Wood);
 
-		//test materials:
+		pm->SetMaterialInteraction("Aluminum", "Steel", matDesc_Aluminum_Steel);
 
-		pm->NewMaterial("Normal",0.5f, 0.5f, 0.5f);
+		// ------------ Test materials ------------
+		// Normal
+		matDesc_Normal.m_fStatic = 0.5f;
+		matDesc_Normal.m_fKinetic = 0.5f;
+		matDesc_Normal.m_fRestitution = 0.5f;
+		// Jumpy
+		matDesc_Jumpy.m_fStatic = 1.0f;
+		matDesc_Jumpy.m_fKinetic = 1.0f;
+		matDesc_Jumpy.m_fRestitution = 1.0f;
+		// Stubborn
+		matDesc_Stubborn.m_fStatic = 1.0f;
+		matDesc_Stubborn.m_fKinetic = 1.0f;
+		matDesc_Stubborn.m_fRestitution = 0.0f;
+		// LittleR
+		matDesc_LittleR.m_fStatic = 1.0f;
+		matDesc_LittleR.m_fKinetic = 1.0f;
+		matDesc_LittleR.m_fRestitution = 0.3f;
+		// Sliding
+		matDesc_Sliding.m_fStatic = 1.0f;
+		matDesc_Sliding.m_fKinetic = 0.1f;
+		matDesc_Sliding.m_fRestitution = 1.0f;
+		// Unmoving
+		matDesc_Unmoving.m_fStatic = 1.0f;
+		matDesc_Unmoving.m_fKinetic = 100.0f;
+		matDesc_Unmoving.m_fRestitution = 1.0f;
+		// LittleK
+		matDesc_LittleK.m_fStatic = 1.0f;
+		matDesc_LittleK.m_fKinetic = 0.9f;
+		matDesc_LittleK.m_fRestitution = 1.0f;
+		// Smooth
+		matDesc_Smooth.m_fStatic = 0.1f;
+		matDesc_Smooth.m_fKinetic = 1.0f;
+		matDesc_Smooth.m_fRestitution = 1.0f;
+		// Sticky
+		matDesc_Sticky.m_fStatic = 10.0f;
+		matDesc_Sticky.m_fKinetic = 1.0f;
+		matDesc_Sticky.m_fRestitution = 1.0f;
+		// LittleS
+		matDesc_LittleS.m_fStatic = 0.2f;
+		matDesc_LittleS.m_fKinetic = 1.0f;
+		matDesc_LittleS.m_fRestitution = 1.0f;
+		// Normal-Jumpy
+		matDesc_NormalJumpy.m_fStatic = 1.0f;
+		matDesc_NormalJumpy.m_fKinetic = 1.0f;
+		matDesc_NormalJumpy.m_fRestitution = 1.0f;
+		// Normal-Stubborn
+		matDesc_NormalStubborn.m_fStatic = 1.0f;
+		matDesc_NormalStubborn.m_fKinetic = 1.0f;
+		matDesc_NormalStubborn.m_fRestitution = 0.0f;
+		// Normal-LittleR
+		matDesc_NormalLittleR.m_fStatic = 1.0f;
+		matDesc_NormalLittleR.m_fKinetic = 1.0f;
+		matDesc_NormalLittleR.m_fRestitution = 0.3f;
+
+		pm->NewMaterial("Normal", matDesc_Normal);
 		//test restitution
-		pm->NewMaterial("Jumpy",1, 1, 1);
-		pm->NewMaterial("Stubborn", 1, 1, 0);
-		pm->NewMaterial("LittleR", 1,1,0.3);
+		pm->NewMaterial("Jumpy", matDesc_Jumpy);
+		pm->NewMaterial("Stubborn", matDesc_Stubborn);
+		pm->NewMaterial("LittleR", matDesc_LittleR);
 
-		pm->SetMaterialInteraction("Normal", "Jumpy",1, 1, 1);
-		pm->SetMaterialInteraction("Normal", "Stubborn", 1, 1, 0);
-		pm->SetMaterialInteraction("Normal", "LittleR", 1, 1, 0.3);
+		pm->SetMaterialInteraction("Normal", "Jumpy", matDesc_NormalJumpy);
+		pm->SetMaterialInteraction("Normal", "Stubborn", matDesc_NormalStubborn);
+		pm->SetMaterialInteraction("Normal", "LittleR", matDesc_NormalLittleR);
 
 		//test kinetic friction
-		pm->NewMaterial("Sliding", 1, 0.1,1);
-		pm->NewMaterial("Unmoving", 1, 100, 1);
-		pm->NewMaterial("LittleK", 1,0.9,1);
+		pm->NewMaterial("Sliding", matDesc_Sliding);
+		pm->NewMaterial("Unmoving", matDesc_Unmoving);
+		pm->NewMaterial("LittleK", matDesc_LittleK);
 
 		//test static friction
-		pm->NewMaterial("Smooth", 0.1, 1, 1);
-		pm->NewMaterial("Sticky", 10, 1, 1);
-		pm->NewMaterial("LittleS", 0.2, 1, 1);
+		pm->NewMaterial("Smooth", matDesc_Smooth);
+		pm->NewMaterial("Sticky", matDesc_Sticky);
+		pm->NewMaterial("LittleS", matDesc_LittleS);
 
-		pTerrain->SetMaterial(pm->GetMaterial("Normal") );
+		if ( pTerrain == NULL ) {
+			printf("Error: Cannot choose no terrain for the Material test\n");
+		}
+		
+		pTerrain->SetMaterial( pm->GetMaterial("Normal") );
 	}
 
 void Test_3::Input(SDL_Event E) {
-		int i,j;
+		
 		palBodyBase *pb= NULL;
 		palMaterials *pm = PF->CreateMaterials();
 		switch(E.type) {
--- paldemo/test3.h
+++ paldemo/test3.h
@@ -28,6 +28,30 @@ protected:
 	void InitMaterials();
 	palBody *pbM[3];
 	FACTORY_CLASS(Test_3,Test_3,palTests,2);
+
+	// Properties for real material types
+	palMaterialDesc matDesc_Aluminium;
+	palMaterialDesc matDesc_Diamond;
+	palMaterialDesc matDesc_Oak;
+	palMaterialDesc matDesc_Steel;
+	palMaterialDesc matDesc_Wood;
+	palMaterialDesc matDesc_Aluminum_Steel;
+
+	// Properties for other material types
+	palMaterialDesc matDesc_Normal;
+	palMaterialDesc matDesc_Jumpy;
+	palMaterialDesc matDesc_Stubborn;
+	palMaterialDesc matDesc_LittleR;
+	palMaterialDesc matDesc_Sliding;
+	palMaterialDesc matDesc_Unmoving;
+	palMaterialDesc matDesc_LittleK;
+	palMaterialDesc matDesc_Smooth;
+	palMaterialDesc matDesc_Sticky;
+	palMaterialDesc matDesc_LittleS;
+	// Interaction types
+	palMaterialDesc matDesc_NormalJumpy;
+	palMaterialDesc matDesc_NormalStubborn;
+	palMaterialDesc matDesc_NormalLittleR;
 };
 
 #endif
