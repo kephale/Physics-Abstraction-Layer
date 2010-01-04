9e78f91236ada1fc1f2018ec89b1c75a62d36ce1
aclong <aclong@99ea70e1-9a43-0410-b3df-a6f06dae6e96>
2010-01-04 02:00:48 +0000

    added initization to member in palBulletDebugDraw

--- pal_i/bullet_pal.cpp
+++ pal_i/bullet_pal.cpp
@@ -96,6 +96,9 @@ FACTORY_CLASS_IMPLEMENTATION_END_GROUP;
 
 class palBulletDebugDraw : public btIDebugDraw
 {
+public:
+        palBulletDebugDraw() : m_pPalDebugDraw(0) {}
+  
 	virtual void drawLine(const btVector3& from, const btVector3& to,
 			const btVector3& color) {
 		if (m_pPalDebugDraw) {
