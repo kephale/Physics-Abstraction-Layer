dddd8042b3334801c0bfc9a61d45b46f554aae5f
aclong <aclong@99ea70e1-9a43-0410-b3df-a6f06dae6e96>
2010-07-27 21:10:23 +0000

    added exception classes

---
+++ pal/palException.cpp
@@ -0,0 +1,47 @@
+/*
+ * palException.cpp
+ *
+ *  Created on: Jul 27, 2010
+ *      Author: Chris Long
+ *  Copyright (C) 2010 SET Corporation
+ */
+
+#include <pal/palException.h>
+#include <string.h>
+
+palException::palException() throw()
+	: m_message(0), m_cause(0)
+{
+}
+
+palException::palException(const char* message, std::exception *cause) throw()
+	: m_message(message), m_cause(cause)
+{
+}
+
+palException::~palException() throw() {
+	delete m_message;
+	m_message = 0;
+	delete m_cause;
+	m_cause = 0;
+}
+
+std::exception *palException::GetCause() const {
+	return m_cause;
+}
+
+const char *palException::what() const throw() {
+	return m_message;
+}
+
+void palException::InitMessage(const char *message) {
+	if (!m_message) {
+		m_message = message;
+	}
+}
+
+void palException::InitCause(std::exception *cause) {
+	if (!m_cause) {
+		m_cause = cause;
+	};
+}
---
+++ pal/palException.h
@@ -0,0 +1,35 @@
+/*
+ * palException.hh
+ *
+ *  Created on: Jul 27, 2010
+ *      Author: Chris Long
+ *  Copyright (C) 2010 SET Corporation
+ */
+
+#ifndef PALEXCEPTION_HH_
+#define PALEXCEPTION_HH_
+
+#include <stdexcept>
+
+class palException : public std::exception {
+public:
+	palException() throw();
+	palException(const char* message, std::exception* cause = 0) throw();
+	virtual ~palException() throw();
+	virtual const char* what() const throw();
+	virtual void InitMessage(const char* message);
+	virtual void InitCause(std::exception* cause);
+	virtual std::exception* GetCause() const;
+protected:
+	const char* m_message;
+	std::exception* m_cause;
+};
+
+class palIllegalStateException : public palException {
+public:
+	palIllegalStateException() throw();
+	palIllegalStateException(const char* message, std::exception* cause = 0) throw() :
+		palException(message, cause) {}
+};
+
+#endif /* PALEXCEPTION_HH_ */
